9e822e730fe8a2a03a5887044fb3a5da23969fbe
Christopher <cfelton@Christophers-MacBook-Pro.local>
2017-12-25 07:19:31 -0600

    added fir filters

---
+++ filter_blocks/fir/fir_parallel.py
@@ -0,0 +1,42 @@
+
+from math import log, ceil
+
+import myhdl as hdl
+from myhdl import Signal, intbv, always_seq
+
+
+@hdl.block
+def filter_fir_parallel(clock, reset, x, y, h):
+    """ A basic FIR filter description
+    """
+
+    xd = [Signal(intbv(0, min=x.data.min, max=x.data.max))
+          for _ in range(len(h)-1)]
+
+    # need to scale the outputs, the multiply will
+    # create a number twice as big
+    scale = int(len(x.data)-1)
+
+    @always_seq(clock.posedge, reset=reset)
+    def beh_sop():
+        if x.vld:
+            # tap update loop
+            xd[0].next = x.sig
+            for ii in range(1, len(h)-1):
+                xd[ii].next = xd[ii-1]
+                
+            # sum-of-products loop
+            c = h[0]
+            sop = x.data * c
+            for ii in range(len(h)-1):
+                c = h[ii+1]
+                sop = sop + (c * xd[ii])
+            
+            # scale the sum of products to the 
+            # output range (truncate)
+            y.data.next = sop >> scale
+            y.valid.next = True
+        else:
+            y.valid.next = False
+
+    return beh_sop
