146b4707403ab8448c99cd28b196c5a7528d7727
Sriyash Caculo <sriyash25@gmail.com>
2018-07-03 02:12:41 +0530

    replaced concat vector with list

--- filter_blocks/iir/iir_parallel.py
+++ filter_blocks/iir/iir_parallel.py
@@ -1,10 +1,10 @@
 import myhdl as hdl
-from myhdl import Signal, intbv, always_seq, ConcatSignal
+from myhdl import Signal, intbv
 from filter_blocks.support import Samples, Signals
 
 
 @hdl.block
-def filter_iir(glbl, sigin, sigout, y_valid, b, a, shared_multiplier=False):
+def filter_iir(glbl, sigin, sigout, b, a, shared_multiplier=False):
     """Parallel IIR filter.
 
     Ports:
@@ -41,8 +41,8 @@ def filter_iir(glbl, sigin, sigout, y_valid, b, a, shared_multiplier=False):
     # Locally reference the interface signals
     clock, reset = glbl.clock, glbl.reset
     xdv = sigin.valid
-    ydv = y_valid
-    y = sigout
+    ydv = sigout.valid
+    y = sigout.data
     x = Signal(intbv(0, min=vmin, max=vmax))
 
     # Delay elements, list-of-signals (double length for all)
@@ -56,48 +56,51 @@ def filter_iir(glbl, sigin, sigout, y_valid, b, a, shared_multiplier=False):
     @hdl.always(clock.posedge)
     def beh_direct_form_one():
         if sigin.valid:
-            x.next=sigin.data
+            x.next = sigin.data
             ffd[1].next = ffd[0]
             ffd[0].next = x
 
             fbd[1].next = fbd[0]
             fbd[0].next = yacc.signed()
             #print(fbd[0])
-            
+
     @hdl.always_comb
     def beh_acc():
-         #double precision accumulator 
+         #double precision accumulator
         yacc.next = (b0 * x) + (b1 * ffd[0]) + (b2 * ffd[1]) \
                     - (a1 * fbd[0]) - (a2 * fbd[1])
-       
-       
+
+
     @hdl.always_seq(clock.posedge, reset=reset)
     def beh_output():
         dvd.next = xdv
         y.next = yacc.signed()
         ydv.next = dvd
+        #print(y)
 
     return beh_direct_form_one, beh_acc, beh_output
 
 
 
 @hdl.block
-def parallel_sum(glbl, b, y_len, y_valid, yin, yout):
+def parallel_sum(glbl, b, yin, yout):
 
     """sum of all the individial parallel outputs ...
     """
     clock, reset = glbl.clock, glbl.reset
-    k= Signal(intbv(0)[8:])
+    k = Signal(intbv(0)[8:])
     yout = Samples(k.min, k.max)
-    
+    list_of_data = [ds.data for ds in yin]
+    list_of_valid = [ds.valid for ds in yin]
+
+
     @hdl.always_seq(clock.posedge, reset=reset)
     def output():
         yout.data = 0
         for ii in range(len(b)):
-            yout.data=yout.data+yin[(ii+1)*y_len:ii*y_len]
+            yout.data = yout.data+list_of_data[ii]
 
         print(yout.data)
-        #print(y_valid)
 
     return output
 
@@ -114,23 +117,15 @@ def filter_iir_parallel(glbl, x, y, b, a, w):
     vmax = 2**(2*w[0])  # double width max and min
     vmin = -vmax
 
-    #y_i = Signal[(intbv(0)[20:])for _ in range(number_of_sections)]
-    y_i = Signals(intbv(0, min=vmin, max=vmax), number_of_sections)
-    y_valid = [Signal(bool(0)) for _ in range(number_of_sections)]
-    y_len = len(y_i[0])
-    
-    #for i in y_i:
-    #    print(y_i)
-    
-    yvector = ConcatSignal(*reversed(y_i))
-    y_valid_vector = ConcatSignal(*reversed(y_valid))
+    y_i = [Samples(vmin, vmax) for _ in range(number_of_sections)]
 
-    for ii in range(len(b)):    
+    for ii in range(len(b)):
         list_of_insts[ii] = filter_iir(
-            glbl, x, y_i[ii], y_valid[ii],
-            b=tuple(map(int, b[ii])),  a=tuple(map(int, a[ii])))
+            glbl, x, y_i[ii],
+            b=tuple(map(int, b[ii])), a=tuple(map(int, a[ii])))
 
-    insts = parallel_sum(glbl, b, y_len, y_valid_vector, yvector, y)
+
+    insts = parallel_sum(glbl, b, y_i, y)
 
 
     return list_of_insts, insts
